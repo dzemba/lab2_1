be095a5970c8a0002810e26f481017a33bd56aae
Rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-03-13 09:56:13 +0100

    Po daodaniu drugiego testu i pomocy pana adamusa moja drugi test jednostkowy zaczol dzialas, mniej uzywac slow przecinkowych.

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -6,13 +6,22 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 	
-	public static void testEqualsOneIsInsane() throws Exception{
-		BinarySearchTest searchTest = new BinarySearchTest();
+	@Test
+	public void testEqualsOneIsInsane() throws Exception{
 		int[] sequency = new int[1];
 		int key = 5;
 		sequency[0] = key;
 		SearchResult searchResult = BinarySearch.search(key,sequency);
 		assertTrue(searchResult.isFound());
+	}
+	
+	@Test
+	public void testEqualsOneIsNotInsane() throws Exception{
+		int[] sequency = new int[1];
+		int key = 5;
+		//sequency[0] = key;
+		SearchResult searchResult = BinarySearch.search(key,sequency);
+		assertFalse(searchResult.isFound());
 	}
 	
 	
