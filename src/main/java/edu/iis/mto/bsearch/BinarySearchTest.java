90b52b46d2e82ab692cb0344d3d62cf7ebe0b41a
Rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-03-13 10:25:12 +0100

    Napisalem 3 test

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -2,6 +2,8 @@ package edu.iis.mto.bsearch;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 public class BinarySearchTest {
@@ -23,6 +25,25 @@ public class BinarySearchTest {
 		SearchResult searchResult = BinarySearch.search(key,sequency);
 		assertFalse(searchResult.isFound());
 	}
+	
+	@Test
+	public void testEMorThanOneIsFirstInSequence() throws Exception{
+		
+		int sequencyLenght = 7;
+		int[] sequency = new int[sequencyLenght];
+		int key = 1;
+		sequency[0] = key;
+		
+		Random generator = new Random();
+		for(int iterator = 1; iterator < sequencyLenght; iterator++){
+			sequency[iterator] = generator.nextInt(10)+3;
+		}
+		
+		sequency[0] = key;
+		SearchResult searchResult = BinarySearch.search(key,sequency);
+		assertEquals(1,searchResult.getPosition());
+	}
+	
 	
 	
 	@Test
